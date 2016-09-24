15793330ead91dea46f6d5f75eeed88a4abc42be
Karpovich Vladimir <dev.karpovich@gmail.com>
2016-09-24 14:40:56 +0500

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install -y squid
-apt-get install curl
+apt-get install -y curl
 rm /etc/squid/squid.conf
 curl https://raw.githubusercontent.com/zloadmin/squid_install/master/squid.conf > /etc/squid/squid.conf
 update-rc.d squid defaults
