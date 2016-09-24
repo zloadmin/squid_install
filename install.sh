fda8237d7a029df03d2111fdace10b8830fce147
Karpovich Vladimir <dev.karpovich@gmail.com>
2016-09-24 14:31:48 +0500

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install -y squid
-mv /etc/squid/squid.conf
+rm /etc/squid/squid.conf
 curl https://raw.githubusercontent.com/zloadmin/squid_install/master/squid.conf > /etc/squid/squid.conf
 update-rc.d squid defaults
 service squid start
