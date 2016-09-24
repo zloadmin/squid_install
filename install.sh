ff7f2b934fbdb4ea78b9ed9d4bb2ac3e1fe7f991
Karpovich Vladimir <dev.karpovich@gmail.com>
2016-09-24 15:19:11 +0500

    fix

--- install.sh
+++ install.sh
@@ -4,5 +4,9 @@ apt-get install -y squid
 apt-get install -y curl
 rm /etc/squid/squid.conf
 curl https://raw.githubusercontent.com/zloadmin/squid_install/master/squid.conf > /etc/squid/squid.conf
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_TYPE=en_US.UTF-8
 update-rc.d squid defaults
 service squid start
