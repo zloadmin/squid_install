56060384038b7db6d51b3b026d9a0b869f663d3d
Karpovich Vladimir <dev.karpovich@gmail.com>
2016-09-24 15:28:19 +0500

    add echo

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+echo 'Update packeges'
 apt-get update
-apt-get install -y squid
-apt-get install -y curl
-rm /etc/squid/squid.conf
-curl https://raw.githubusercontent.com/zloadmin/squid_install/master/squid.conf > /etc/squid/squid.conf
+echo 'Setting variables'
 export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_TYPE=en_US.UTF-8
+echo 'Install squid'
+apt-get install -y squid
+echo 'Install curl'
+apt-get install -y curl
+echo 'Add new config'
+curl https://raw.githubusercontent.com/zloadmin/squid_install/master/squid.conf > /etc/squid/squid.conf
+echo 'Add squid to autoload'
 update-rc.d squid defaults
+echo 'Start squid'
 service squid start
